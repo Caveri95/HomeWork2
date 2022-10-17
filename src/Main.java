ad2a6e74b12bfc6b108473fdb3091b71118313d4
helloworld <ura22051995@gmail.com>
2022-10-17 19:40:57 +0300

    Задача 1

--- src/Main.java
+++ src/Main.java
@@ -1,23 +1,36 @@
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача 1");
         var dog = 8;
         var cat = 3.6;
         var paper = 763789;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("Задача 2");
         dog = dog + 4;
         cat = cat + 4;
         paper = paper + 4;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("Задача 3");
         dog = dog - 4;
         cat = cat - 3;
         paper = paper - 7639;
         System.out.println(dog);
         System.out.println(cat);
         System.out.println(paper);
+        System.out.println("Задача 4");
+        var friend = 19;
+        System.out.println(friend);
+        friend = friend + 2;
+        System.out.println(friend);
+        friend = friend/7;
+        System.out.println(friend);
+        System.out.println("Задача 5");
+        var frog = 3.5;
+        System.out.println(frog);
 
 
     }
